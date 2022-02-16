49740a1fd2c942f05ed7f760b27c02fa7065e9c6
Ashish Bapat <91781811+ashishbapat01@users.noreply.github.com>
2022-02-16 20:24:59 +0530

    Create 1_Input_string_and_print_it.c

---
+++ string/1_Input_string_and_print_it.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	char str[100];
+	
+	printf("\nEnter the string here:-");
+	gets(str);
+	
+	printf("The string you entered is:-%s",str);
+	return 0;
+}
