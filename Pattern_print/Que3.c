2265f71a7c9e5b6e24e20c9773019b6c3fe6cd8c
Ashish Bapat <91781811+ashishbapat01@users.noreply.github.com>
2022-03-10 19:00:53 +0530

    Add files via upload

---
+++ Pattern_print/Que3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int row=0,x=0,num=10;
+	
+	for(row=1;row<=num;row++)
+	{
+		for(x=1;x<=num;x++)
+		{
+			printf("%d",x);
+			
+		}
+		printf("\n");
+	}
+	return 0;
+}
