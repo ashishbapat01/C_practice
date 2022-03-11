cc395f288c38dfa75dbb19cd32e6812f3ab1f3ad
Ashish Bapat <91781811+ashishbapat01@users.noreply.github.com>
2022-03-11 18:22:31 +0530

    Add files via upload

---
+++ Pattern_print/Que6.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int i=1,j=0,num=0,n=10;
+	printf ("Enter Any Number :- ");
+	scanf("%d",&num);
+	
+      for(int i=1;i<=num;i++)	
+	{
+	   
+		for(int j=1;j<=num;j++)
+
+		{
+			printf("%d",n);
+		
+		}
+			n--;
+		
+	printf("\n");
+  }
+	
+	
+	return 0;
+}
