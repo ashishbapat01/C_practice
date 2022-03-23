d67cf9f51e4844b112727ca9fc68a9730f45273d
Ashish Bapat <91781811+ashishbapat01@users.noreply.github.com>
2022-03-23 19:35:49 +0530

    Add files via upload

---
+++ Pattern_print/Que34.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int i=0,j=0,k=0,num=5;
+	
+	for(i=1;i<=num;i++)
+	{
+		for(j=1;j<=num-i;j++)
+		{
+			printf(" ");
+		}
+	for(k=1;k<2*i;k++)	
+	{
+		printf("*");
+	}
+	 printf("\n");
+	}
+	return 0;
+}
